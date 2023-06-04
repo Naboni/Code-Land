0442ba17f2aacd5e26bf2eb4bdd2c8b402ec371c
Naboni <se.naboni.abebe@gmail.com>
2023-06-04 04:05:54 +0300

    created ui for statistics and data tables

--- admin-dashboard/src/App.js
+++ admin-dashboard/src/App.js
@@ -1,4 +1,4 @@
-import { Switch, Route, Redirect } from "react-router-dom";
+import { Switch, Route, Redirect, BrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import Tables from "./pages/Tables";
 import Profile from "./pages/Profile";
@@ -8,21 +8,29 @@ import Main from "./components/layout/Main";
 import "antd/dist/antd.css";
 import "./assets/styles/main.css";
 import "./assets/styles/responsive.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
 
 function App() {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
-    <div className="App">
-      <Switch>
-        <Route path="/sign-up" exact component={SignUp} />
-        <Route path="/sign-in" exact component={SignIn} />
-        <Main>
-          <Route exact path="/dashboard" component={Home} />
-          <Route exact path="/tables" component={Tables} />
-          <Route exact path="/profile" component={Profile} />
-          <Redirect from="*" to="/dashboard" />
-        </Main>
-      </Switch>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <BrowserRouter>
+          <Switch>
+            <Route path="/sign-up" exact component={SignUp} />
+            <Route path="/sign-in" exact component={SignIn} />
+            <Main>
+              <Route exact path="/dashboard" component={Home} />
+              <Route exact path="/tables" component={Tables} />
+              <Route exact path="/profile" component={Profile} />
+              <Redirect from="*" to="/dashboard" />
+            </Main>
+          </Switch>
+        </BrowserRouter>
+      </div>
+    </QueryClientProvider>
   );
 }
 
